44c13c733e7206efcff10a589b71a8de20580d08
rajnish42413 <rajnish42413@gmail.com>
2018-08-27 19:45:15 +0530

    jfjf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { loginService } from "./services/login.service";
+import { dashboardService } from "./services/dashboard.service";
+import { IndexComponent } from "./components/index.component";
+import { loginComponent } from "./components/login.component";
+import { DashboardComponent } from "./components/dashboard.component";
+import { AboutComponent } from "./components/about.component";
+import { ContactComponent } from "./components/contact.component";
+import { PortfolioComponent } from "./components/portfolio.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./routes/app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    loginComponent,
+    DashboardComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [loginService,dashboardService],
+  bootstrap: [IndexComponent]
 })
 export class AppModule { }
